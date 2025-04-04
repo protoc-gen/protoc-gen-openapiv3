6c2496a5740de3417b5b0ef5eeb4fb196e3974ae
ifuryst <ifuryst@gmail.com>
2025-04-04 16:01:12 +0800

    feat: Support optional in proto3

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/protoc-gen/protoc-gen-openapiv3/openapiv3"
+	gengo "google.golang.org/protobuf/cmd/protoc-gen-go/internal_gengo"
 	"google.golang.org/protobuf/compiler/protogen"
 )
 
 func main() {
 	protogen.Options{}.Run(func(gen *protogen.Plugin) error {
 		openapiv3.GenerateFile(gen)
+		gen.SupportedFeatures = gengo.SupportedFeatures
 		return nil
 	})
 }
